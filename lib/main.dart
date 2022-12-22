c0bfd539e36fc8c5658532f57f43867ac19b9e73
cecil <cecilghimire@gmail.com>
2022-12-22 22:17:10 +0545

    adding appbar text body widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
-
 import 'package:flutter/material.dart';
-void main(){
-   runApp (Myapp());
- }
- 
+// void main(){
+//    runApp (Myapp());
+//  }
+
+void main() => runApp(Myapp());
 
 // ignore: use_key_in_widget_constructors
 class Myapp extends StatelessWidget {
-  
-     Widget build(BuildContext ctx){
-       return MaterialApp(home: Text('Hello, cecil! How are you?',));
-   }
-
- }
+  @override
+  Widget build(BuildContext ctx) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('FTX bitcoin')),
+        body: Text('This is my app body'),
+      ),
+    );
+  }
+}
