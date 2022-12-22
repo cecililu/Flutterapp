de1f57646da53390f73ea95e1df8a3b8ad5252e7
cecil <cecilghimire@gmail.com>
2022-12-22 22:41:37 +0545

    Elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+
 // void main(){
 //    runApp (Myapp());
 //  }
-
 void main() => runApp(Myapp());
 
 // ignore: use_key_in_widget_constructors
 class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
+    var question = ['what is your fave color', 'Who is your Favourite person'];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('FTX bitcoin')),
-        body: Text('This is my app body'),
+        
+        body: Column(children: [
+          Text('Here are your question?'),
+          ElevatedButton(child: Text("Aswew2"),onPressed: () {},),
+          ElevatedButton(child: Text("Aswew3"),onPressed: () {},),
+          ElevatedButton(child: Text("Aswew4"),onPressed: () {},),
+          
+        ]),
       ),
     );
   }
