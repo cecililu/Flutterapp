9eb080c726a71e09b8ad63a0fbe64b7b49ddfa06
cecil <cecilghimire@gmail.com>
2022-12-23 19:01:56 +0545

    styling basics question Widget added

--- lib/question.dart
+++ lib/question.dart
@@ -4,11 +4,20 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
-   Question({super.key, required this.questionText});
-  
+  final String questionText;
+  const Question({super.key, required this.questionText});
+
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+        height: 40,
+        width: double.infinity,
+        margin: EdgeInsets.all(20),
+        color: Color.fromARGB(124, 45, 75, 5),
+        child: Text(
+          questionText,
+          style: const TextStyle(fontSize: 25),
+          textAlign: TextAlign.center,
+        ));
   }
 }
