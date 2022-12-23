ac06fcace692dc23e60aa51d9b713d0ad7016f83
cecil <cecilghimire@gmail.com>
2022-12-23 13:09:36 +0545

    stateless to statefull widget coversion

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,41 @@ import 'package:flutter/material.dart';
 //  }
 void main() => runApp(Myapp());
 
-// ignore: use_key_in_widget_constructors
-class Myapp extends StatelessWidget {
-  void answerQuestion(){
-   print("Answer chosen ok");
+
+class Myapp extends StatefulWidget {
+    @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyappState();
+  }
+  }
+  
+
+  
+class MyappState extends State<Myapp>{
+    var questionIndex=0;
+    void answerQuestion(){
+    setState((){
+        questionIndex=questionIndex+1;
+    });
+   
+    print("Answer chosen ok");
+    print(questionIndex); 
   }
   
   @override
   Widget build(BuildContext ctx) {
-    var question = ['what is your fave color', 'Who is your Favourite person'];
+    var question = ['what is your favourite  color?', 'Who is your Favourite person'];
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('FTX bitcoin')),
+        appBar: AppBar(title: Text('Quiz App')),
         
         body: Column(children: [
-          const Text('Here are your question?'),
-           
+           Text(question[questionIndex]),
            ElevatedButton(child: Text("Aswew2"),onPressed: answerQuestion,),
-           ElevatedButton(child: Text("Aswew3"),onPressed: answerQuestion,),
-           ElevatedButton(child: Text("Aswew4"),onPressed: answerQuestion,),
+           ElevatedButton(child: Text("Aswew3"),onPressed: (){},),
+           ElevatedButton(child: Text("Aswew4"),onPressed: (){},),
           
         ]),
       ),
